6bdf9acb2a83337ef2591446edd3aa8e6dc65900
rodvama <rod_valen_ma@hotmail.com>
2020-05-18 22:28:10 -0500

    Maquina Virtual - v1

---
+++ maquinaVirtual.py
@@ -0,0 +1,20 @@
+# Jose Arturo Villalobos A00818214
+# Rodrigo Valencia A00818256
+# Diseno de compiladores
+# Maquina Virtual
+
+from memVirtual import *
+
+CONST_GLOBALES = 'globales'
+CONST_LOCALES = 'locales'
+CONST_TEMPORALES = 'temporales'
+CONST_CONSTANTES = 'constantes'
+
+mem_GLOBAL = memVirtual('global')
+mem_MAIN = memVirtual('main')
+
+# obejota 
+quad = ()
+quadLista = []
+quadIndice = 0
+sigQuadIndice = -1
